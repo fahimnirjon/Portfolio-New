3cfec60f8e5a1fe5ffce2d052c32e13b61357a3d
fahimnirjon <fahimnirjon21@gmail.com>
2024-07-25 16:09:30 +0600

    added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ function Footer() {
             </div>
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
               <p className="text-sm">
-                &copy; 2024 Your Company. All rights reserved.
+                &copy; 2024 Fahim Nirjon. All rights reserved.
               </p>
               <p className="text-sm">Supportive Partner ❤️ Fahim Nirjon</p>
             </div>
